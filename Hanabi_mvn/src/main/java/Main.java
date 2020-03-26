145d6898f5e31ca7e605c928b6619835124ea764
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2020-03-26 14:15:31 +0100

    to chyba dziala

--- Hanabi_mvn/src/main/java/Main.java
+++ Hanabi_mvn/src/main/java/Main.java
@@ -1,6 +1,8 @@
 
 public class Main{
     public static void main(String [] args){
+        Rameczka a = new Rameczka();
+        a.f();
         System.out.println("HelloWorld");
     }
 }
---
+++ Hanabi_mvn/src/main/java/Rameczka.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Rameczka {
+
+    public void f() {
+        JFrame a = new JFrame();
+        a.add(new JButton("yiss"));
+        a.setSize(200, 200);
+        a.setVisible(true);
+    }
+
+}
