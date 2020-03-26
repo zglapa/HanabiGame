7792bc3a272cdea9927e693723b460b5722fed8c
Zofia Glapa <zofia.glapa00@gmail.com>
2020-03-26 14:10:17 +0100

    przycisk

--- Hanabi_mvn/src/main/java/Main.java
+++ Hanabi_mvn/src/main/java/Main.java
@@ -1,6 +1,29 @@
 
-public class Main{
-    public static void main(String [] args){
-        System.out.println("HelloWorld");
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
+    public static void main(String[] args) {
+        launch(args);
     }
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("TCS");
+        button = new Button("Click to access Tanie Czyszczenie Sprzatanie");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout,500,500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
