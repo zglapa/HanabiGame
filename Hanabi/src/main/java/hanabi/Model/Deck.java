d72ca332861fb65b99f50aa8bf0580f52b972bd7
Roch Wojtowicz <rochw00@gmail.com>
2020-04-04 19:12:43 +0200

    deleating zeroes :)

--- Hanabi/src/main/java/hanabi/Model/Deck.java
+++ Hanabi/src/main/java/hanabi/Model/Deck.java
@@ -13,7 +13,7 @@ public final class Deck {
     Deck(boolean shuffle) {
         cards=new ArrayList <>();
         for(int i=0;i<2;++i)
-            for(int j=0;j<4;++j)
+            for(int j=1;j<=4;++j)
                 addAllColors(j);
         addAllColors(1);
         addAllColors(5);
