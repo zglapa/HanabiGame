021044b3f74eb9c4afbda158db0024e1e350d516
Roch Wojtowicz <rochw00@gmail.com>
2020-04-04 21:45:37 +0200

    First window application

--- Hanabi/src/main/java/hanabi/View/App.java
+++ Hanabi/src/main/java/hanabi/View/App.java
@@ -1,9 +1,16 @@
 package hanabi.View;
 
 
+import hanabi.Controller.Controller;
+import hanabi.Model.Board;
+import hanabi.Model.MoveType;
+import hanabi.Model.Player;
+import hanabi.Model.PlayerMove;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -11,6 +18,23 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
+
+    void imba(TextArea text) {
+        Board board = new Board(4, "twoja", "stara", "twoj", "stary");
+        while(true){
+
+            text.appendText(board.toString());
+            MoveType type = MoveType.DISCARD;
+            Player player = board.getPlayers().get(board.getCurrentPlayerIndex());
+            PlayerMove move = new PlayerMove(player,type,1);
+            try{
+                board.action(move);
+            }catch (Exception e){
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
 
     @Override
     public void start(Stage stage) {
@@ -22,6 +46,18 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
          */
+        stage.setTitle("Kozak window");
+        TextArea text=new TextArea("Ale tu sie bedzie dzialo\n");
+        Button button=new Button("Start a inba");
+        button.setOnAction(e -> imba(text));
+
+        StackPane layout=new StackPane();
+        layout.getChildren().addAll(text,button);
+
+        Scene scene=new Scene(layout,500,300);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
     public static void main(String[] args) {
