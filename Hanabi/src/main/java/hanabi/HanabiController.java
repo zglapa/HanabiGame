8590af915dabc422afa7da1b36e46b1412339a9c
Zofia Glapa <zofia.glapa@student.uj.edu.pl>
2020-04-12 20:38:45 +0200

    Cards have textures now!

--- Hanabi/src/main/java/hanabi/HanabiController.java
+++ Hanabi/src/main/java/hanabi/HanabiController.java
@@ -1,6 +1,7 @@
 package hanabi;
 import hanabi.Model.*;
 import hanabi.Model.Board;
+import hanabi.Model.Color;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -9,14 +10,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.input.InputMethodEvent;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Paint;
-import javafx.scene.paint.Stop;
+import javafx.scene.paint.*;
 import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -24,9 +24,12 @@ import java.util.ResourceBundle;
 public class HanabiController implements Initializable {
 
 
-    @FXML Rectangle card1_1,card1_2,card1_3,card1_4,card2_1,card2_2,card2_3,card2_4,card3_1,card3_2,card3_3,card3_4,card4_1,card4_2,card4_3,card4_4;
-    @FXML Rectangle card5_1,card5_2,card5_3,card5_4,card6_1,card6_2,card6_3,card6_4,card7_1,card7_2,card7_3,card7_4,card8_1,card8_2,card8_3,card8_4;
-    @FXML Rectangle card9_1,card9_2,card9_3,card9_4,card10_1,card10_2,card10_3,card10_4;
+    @FXML Rectangle resultCardW,resultCardY,resultCardR,resultCardG,resultCardB,resultCardRB;
+    @FXML Rectangle card1_1,card1_2,card1_3,card1_4, card1_5, card1_6,card2_1,card2_2,card2_3,card2_4,card2_5, card2_6,
+            card3_1,card3_2,card3_3,card3_4,card3_5, card3_6, card4_1,card4_2,card4_3,card4_4,card4_5, card4_6,
+            card5_1,card5_2,card5_3,card5_4,card5_5, card5_6, card6_1,card6_2,card6_3,card6_4,card6_5, card6_6,
+            card7_1,card7_2,card7_3,card7_4,card7_5, card7_6, card8_1,card8_2,card8_3,card8_4,card8_5, card8_6,
+            card9_1,card9_2,card9_3,card9_4,card9_5, card9_6, card10_1,card10_2,card10_3,card10_4,card10_5, card10_6;
     @FXML Label moveHistory;
     @FXML ComboBox<String> hintTypeChoice;
     @FXML ComboBox<Color> colorChoice;
@@ -47,6 +50,14 @@ public class HanabiController implements Initializable {
     @FXML Button playButton;
     @FXML Button discardButton;
     @FXML TextField cardIndex;
+    ArrayList<Rectangle> resultCards;
+    ArrayList<ImagePattern> whites;
+    ArrayList<ImagePattern> yellows;
+    ArrayList<ImagePattern> greens;
+    ArrayList<ImagePattern> blues;
+    ArrayList<ImagePattern> reds;
+    ArrayList<ImagePattern> rainbows;
+    ArrayList<ArrayList<ImagePattern>> allColorLists;
     Integer numberHint;
     Color colorHint;
     Player playerHint;
@@ -59,7 +70,12 @@ public class HanabiController implements Initializable {
     ArrayList<Label> players;
     ArrayList<ArrayList<Rectangle>> cards;
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+        try {
+            addColors();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         players = new ArrayList<>();
         players.add(player1);
         players.add(player2);
@@ -71,10 +87,17 @@ public class HanabiController implements Initializable {
         players.add(player8);
         players.add(player9);
         players.add(player10);
+        resultCards = new ArrayList<>();
         cards = new ArrayList<>();
-        int PLAYERAMOUNT = 6; //thats temporary
-        board = new Board(PLAYERAMOUNT, 40, 8, 8, 0, new Deck(true, true, true), false, Board.randomNames(PLAYERAMOUNT));
+        int PLAYERAMOUNT = 7; //thats temporary
+        boolean WITHRAINBOWS = true; //temporary as well
+        board = new Board(PLAYERAMOUNT, 40, 8, 8, 3, new Deck(true, WITHRAINBOWS, true), false, Board.randomNames(PLAYERAMOUNT));
         addCardsToArrayList(PLAYERAMOUNT);
+        try {
+            addResultCards(WITHRAINBOWS);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         updateHands();
         showYourTrueColors();
         cardIx = 0;
@@ -125,6 +148,7 @@ public class HanabiController implements Initializable {
         updateHands(index);
         blurMe((index+1)%board.getPlayerAmount());
         updateResult();
+        updateResultCards();
         updateDiscardPile();
         updateMoveHistory();
     }
@@ -142,6 +166,7 @@ public class HanabiController implements Initializable {
         updateHands(index);
         blurMe((index+1)%board.getPlayerAmount());
         updateResult();
+        updateResultCards();
         updateDiscardPile();
         updateMoveHistory();
         System.out.println(board.getCurrentPlayerIndex());
@@ -165,6 +190,7 @@ public class HanabiController implements Initializable {
         updateHands(index);
         blurMe((index+1)%board.getPlayerAmount());
         updateResult();
+        updateResultCards();
         updateDiscardPile();
         updateMoveHistory();
         System.out.println(board.getCurrentPlayerIndex());
@@ -180,41 +206,28 @@ public class HanabiController implements Initializable {
         }
     }
     public void blurMe(int index){
-        for(int i = 0; i < 4; ++i){
+        for(int i = 0; i < board.getHandSize(); ++i){
             cards.get(index).get(i).setFill(javafx.scene.paint.Color.DARKSLATEGRAY);
         }
     }
     public void updateHands(int index){
-        for(int i = 0; i < 4; ++i){
-            Color color = board.getPlayers().get(index).getHand().get(i).getColor();
-            javafx.scene.paint.Color paintColor = javafx.scene.paint.Color.WHITE;
-            LinearGradient rainbow = null;
-            if(color==Color.Y) paintColor = javafx.scene.paint.Color.YELLOW;
-            else if(color==Color.R) paintColor = javafx.scene.paint.Color.RED;
-            else if(color==Color.G) paintColor = javafx.scene.paint.Color.LAWNGREEN;
-            else if(color==Color.B) paintColor = javafx.scene.paint.Color.BLUE;
-            else if(color==Color.RAINBOW){
-                Stop[] stops = new Stop[] {
-                        new Stop(0, javafx.scene.paint.Color.RED),
-                        new Stop(0.2, javafx.scene.paint.Color.ORANGE),
-                        new Stop(0.4, javafx.scene.paint.Color.YELLOW),
-                        new Stop(0.6, javafx.scene.paint.Color.LAWNGREEN),
-                        new Stop(0.8, javafx.scene.paint.Color.BLUE),
-                        new Stop(1, javafx.scene.paint.Color.VIOLET)
-                };
-                rainbow = new LinearGradient(0, 0, 1, 0, true, CycleMethod.REPEAT, stops);
-            }
-            if(rainbow == null){
-                cards.get(index).get(i).setFill(paintColor);
-            }
-            else{
-                cards.get(index).get(i).setFill(rainbow);
-            }
+        for(int i = 0; i < board.getHandSize(); ++i){
+            Card card= board.getPlayers().get(index).getHand().get(i);
+            int cardValue = card.getValue();
+            Color color = card.getColor();
+            ImagePattern colorPattern = whites.get(cardValue-1);
+            if(color==Color.Y) colorPattern = yellows.get(cardValue-1);
+            else if(color==Color.R) colorPattern = reds.get(cardValue-1);
+            else if(color==Color.G) colorPattern = greens.get(cardValue-1);
+            else if(color==Color.B) colorPattern = blues.get(cardValue-1);
+            else if(color==Color.RAINBOW) colorPattern = rainbows.get(cardValue-1);
+            cards.get(index).get(i).setFill(colorPattern);
+
         }
     }
     public void showYourTrueColors(){
         blurMe(0);
-        for(int index = 1; index < board.getPlayerAmount(); ++index){
+        for(int index = 1; index < board.getPlayerAmount() ; ++index){
             updateHands(index);
         }
 
@@ -224,6 +237,14 @@ public class HanabiController implements Initializable {
     }
     public void updateResult(){
         result.setText(board.getResult().toString());
+    }
+    public void updateResultCards(){
+        for(int i = 0; i< resultCards.size(); ++i){
+            int cardValue = board.getResult().get(Color.values()[i]);
+            if(cardValue == 0) continue;
+            ImagePattern colorPattern = allColorLists.get(i).get(cardValue-1);
+            resultCards.get(i).setFill(colorPattern);
+        }
     }
     public void updateDiscardPile(){
         discardPile.setText(board.getDiscardPile().toString());
@@ -253,46 +274,137 @@ public class HanabiController implements Initializable {
         cards.get(0).add(card1_2);
         cards.get(0).add(card1_3);
         cards.get(0).add(card1_4);
+        cards.get(0).add(card1_5);
+        cards.get(0).add(card1_6);
         cards.get(1).add(card2_1);
         cards.get(1).add(card2_2);
         cards.get(1).add(card2_3);
         cards.get(1).add(card2_4);
+        cards.get(1).add(card2_5);
+        cards.get(1).add(card2_6);
         cards.get(2).add(card3_1);
         cards.get(2).add(card3_2);
         cards.get(2).add(card3_3);
         cards.get(2).add(card3_4);
+        cards.get(2).add(card3_5);
+        cards.get(2).add(card3_6);
         cards.get(3).add(card4_1);
         cards.get(3).add(card4_2);
         cards.get(3).add(card4_3);
         cards.get(3).add(card4_4);
+        cards.get(3).add(card4_5);
+        cards.get(3).add(card4_6);
         cards.get(4).add(card5_1);
         cards.get(4).add(card5_2);
         cards.get(4).add(card5_3);
         cards.get(4).add(card5_4);
+        cards.get(4).add(card5_5);
+        cards.get(4).add(card5_6);
         cards.get(5).add(card6_1);
         cards.get(5).add(card6_2);
         cards.get(5).add(card6_3);
         cards.get(5).add(card6_4);
+        cards.get(5).add(card6_5);
+        cards.get(5).add(card6_6);
         cards.get(6).add(card7_1);
         cards.get(6).add(card7_2);
         cards.get(6).add(card7_3);
         cards.get(6).add(card7_4);
+        cards.get(6).add(card7_5);
+        cards.get(6).add(card7_6);
         cards.get(7).add(card8_1);
         cards.get(7).add(card8_2);
         cards.get(7).add(card8_3);
         cards.get(7).add(card8_4);
+        cards.get(7).add(card8_5);
+        cards.get(7).add(card8_6);
         cards.get(8).add(card9_1);
         cards.get(8).add(card9_2);
         cards.get(8).add(card9_3);
         cards.get(8).add(card9_4);
+        cards.get(8).add(card9_5);
+        cards.get(8).add(card9_6);
         cards.get(9).add(card10_1);
         cards.get(9).add(card10_2);
         cards.get(9).add(card10_3);
         cards.get(9).add(card10_4);
+        cards.get(9).add(card10_5);
+        cards.get(9).add(card10_6);
+        for(int i = 0; i < numberOfPlayers; ++i){
+            for(int j = board.getHandSize(); j < 6;++j) cards.get(i).get(j).setVisible(false);
+        }
         for(int i = numberOfPlayers; i < 10; ++i){
-            for(int j = 0; j <4; ++j){
+            for(int j = 0; j <6; ++j){
                 cards.get(i).get(j).setVisible(false);
             }
         }
+    }
+    public void addColors() throws URISyntaxException {
+        whites = new ArrayList<>();
+        yellows = new ArrayList<>();
+        reds = new ArrayList<>();
+        greens = new ArrayList<>();
+        blues = new ArrayList<>();
+        rainbows = new ArrayList<>();
+
+        whites.add(new ImagePattern(new Image(getClass().getResource("/Colors/White1.jpg").toURI().toString())));
+        whites.add(new ImagePattern(new Image(getClass().getResource("/Colors/White2.jpg").toURI().toString())));
+        whites.add(new ImagePattern(new Image(getClass().getResource("/Colors/White3.jpg").toURI().toString())));
+        whites.add(new ImagePattern(new Image(getClass().getResource("/Colors/White4.jpg").toURI().toString())));
+        whites.add(new ImagePattern(new Image(getClass().getResource("/Colors/White5.jpg").toURI().toString())));
+
+        yellows.add(new ImagePattern(new Image(getClass().getResource("/Colors/Yellow1.jpg").toURI().toString())));
+        yellows.add(new ImagePattern(new Image(getClass().getResource("/Colors/Yellow2.jpg").toURI().toString())));
+        yellows.add(new ImagePattern(new Image(getClass().getResource("/Colors/Yellow3.jpg").toURI().toString())));
+        yellows.add(new ImagePattern(new Image(getClass().getResource("/Colors/Yellow4.jpg").toURI().toString())));
+        yellows.add(new ImagePattern(new Image(getClass().getResource("/Colors/Yellow5.jpg").toURI().toString())));
+
+        reds.add(new ImagePattern(new Image(getClass().getResource("/Colors/Red1.jpg").toURI().toString())));
+        reds.add(new ImagePattern(new Image(getClass().getResource("/Colors/Red2.jpg").toURI().toString())));
+        reds.add(new ImagePattern(new Image(getClass().getResource("/Colors/Red3.jpg").toURI().toString())));
+        reds.add(new ImagePattern(new Image(getClass().getResource("/Colors/Red4.jpg").toURI().toString())));
+        reds.add(new ImagePattern(new Image(getClass().getResource("/Colors/Red5.jpg").toURI().toString())));
+
+        greens.add(new ImagePattern(new Image(getClass().getResource("/Colors/Green1.jpg").toURI().toString())));
+        greens.add(new ImagePattern(new Image(getClass().getResource("/Colors/Green2.jpg").toURI().toString())));
+        greens.add(new ImagePattern(new Image(getClass().getResource("/Colors/Green3.jpg").toURI().toString())));
+        greens.add(new ImagePattern(new Image(getClass().getResource("/Colors/Green4.jpg").toURI().toString())));
+        greens.add(new ImagePattern(new Image(getClass().getResource("/Colors/Green5.jpg").toURI().toString())));
+
+        blues.add(new ImagePattern(new Image(getClass().getResource("/Colors/Blue1.jpg").toURI().toString())));
+        blues.add(new ImagePattern(new Image(getClass().getResource("/Colors/Blue2.jpg").toURI().toString())));
+        blues.add(new ImagePattern(new Image(getClass().getResource("/Colors/Blue3.jpg").toURI().toString())));
+        blues.add(new ImagePattern(new Image(getClass().getResource("/Colors/Blue4.jpg").toURI().toString())));
+        blues.add(new ImagePattern(new Image(getClass().getResource("/Colors/Blue5.jpg").toURI().toString())));
+
+        rainbows.add(new ImagePattern(new Image(getClass().getResource("/Colors/RB1.jpg").toURI().toString())));
+        rainbows.add(new ImagePattern(new Image(getClass().getResource("/Colors/RB2.jpg").toURI().toString())));
+        rainbows.add(new ImagePattern(new Image(getClass().getResource("/Colors/RB3.jpg").toURI().toString())));
+        rainbows.add(new ImagePattern(new Image(getClass().getResource("/Colors/RB4.jpg").toURI().toString())));
+        rainbows.add(new ImagePattern(new Image(getClass().getResource("/Colors/RB5.jpg").toURI().toString())));
+
+        allColorLists = new ArrayList<>();
+
+        allColorLists.add(whites);
+        allColorLists.add(yellows);
+        allColorLists.add(reds);
+        allColorLists.add(greens);
+        allColorLists.add(blues);
+        allColorLists.add(rainbows);
+    }
+    public void addResultCards(boolean rainbowCard) throws URISyntaxException {
+        resultCardW.setFill(new ImagePattern(new Image(getClass().getResource("/Colors/White.jpg").toURI().toString())));
+        resultCardY.setFill(new ImagePattern(new Image(getClass().getResource("/Colors/Yellow.jpg").toURI().toString())));
+        resultCardR.setFill(new ImagePattern(new Image(getClass().getResource("/Colors/Red.jpg").toURI().toString())));
+        resultCardG.setFill(new ImagePattern(new Image(getClass().getResource("/Colors/Green.jpg").toURI().toString())));
+        resultCardB.setFill(new ImagePattern(new Image(getClass().getResource("/Colors/Blue.jpg").toURI().toString())));
+        resultCardRB.setFill(new ImagePattern(new Image(getClass().getResource("/Colors/RB.jpg").toURI().toString())));
+        resultCards.add(resultCardW);
+        resultCards.add(resultCardY);
+        resultCards.add(resultCardR);
+        resultCards.add(resultCardG);
+        resultCards.add(resultCardB);
+        if(!rainbowCard) resultCardRB.setVisible(false);
+        else resultCards.add(resultCardRB);
     }
 }
