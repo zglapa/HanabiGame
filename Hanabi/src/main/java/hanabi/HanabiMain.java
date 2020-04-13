7eee94b556f976ba694000abc7b1e4c1b5b38301
Roch Wojtowicz <rochw00@gmail.com>
2020-04-13 09:56:53 +0000

    Adding setup to main

--- Hanabi/src/main/java/hanabi/HanabiMain.java
+++ Hanabi/src/main/java/hanabi/HanabiMain.java
@@ -7,12 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HanabiMain extends Application {
-    Parent root;
+    Parent root,setUpRoot;
     public static void main(String[] args){
         launch(args);
     }
     @Override
     public void start(Stage stage) throws Exception {
+        /*setUpRoot = FXMLLoader.load(getClass().getResource("/setUpFXML.fxml"));
+        stage.setTitle("Set Up");
+        stage.setScene(new Scene(setUpRoot, 1200, 600));
+        stage.show();*/
+
         root = FXMLLoader.load(getClass().getResource("/GameFXML.fxml"));
         stage.setTitle("Hello World");
         stage.setScene(new Scene(root, 1200, 600));
