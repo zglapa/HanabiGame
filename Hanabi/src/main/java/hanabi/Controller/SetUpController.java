adccc1f5d33c8d20bc06dc2f1c94ae48cb97bb78
Roch Wojtowicz <rochw00@gmail.com>
2020-04-16 19:40:06 +0000

    Updates to Set Up, names not working, but looking pretty

--- Hanabi/src/main/java/hanabi/Controller/SetUpController.java
+++ Hanabi/src/main/java/hanabi/Controller/SetUpController.java
@@ -4,35 +4,76 @@ import hanabi.Model.Board;
 import hanabi.Model.Deck;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
-public class SetUpController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class SetUpController implements Initializable {
     @FXML Slider numOfPlayers;
-    @FXML Slider randomOrder;
     @FXML Slider numOfCards;
-    @FXML Slider hasRainbows;
+    @FXML CheckBox randomOrder;
+    @FXML CheckBox hasRainbows;
     @FXML CheckBox showAdvanced;
-    @FXML Button play;
-    @FXML Pane advancedSettings;
+    @FXML VBox advancedSettings;
+    @FXML CheckBox randomNames;
+    @FXML VBox namesBox;
+    @FXML TextField name1;
+    @FXML TextField name2;
+    @FXML TextField name3;
+    @FXML TextField name4;
+    @FXML TextField name5;
+    @FXML TextField name6;
+    @FXML TextField name7;
+    ArrayList<TextField> names;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        names=new ArrayList<>();
+        names.add(name1);
+        names.add(name2);
+        names.add(name3);
+        names.add(name4);
+        names.add(name5);
+        names.add(name6);
+        names.add(name7);
+    }
 
     public void adjustCards(MouseEvent mouseEvent) {
-        if(numOfPlayers.getValue()<=3.0)
+        double players=numOfPlayers.getValue();
+        if(players<=3.0)
             numOfCards.setValue(5.0);
-        else if(numOfPlayers.getValue()<=5.0)
+        else if(players<=5.0)
             numOfCards.setValue(4.0);
         else
             numOfCards.setValue(3.0);
+
+        for(int i=0;i<7;++i) {
+            names.get(i).setVisible(i<players);
+        }
+    }
+
+    public void advanced(MouseEvent mouseEvent) {
+        advancedSettings.setVisible(showAdvanced.isSelected());
+    }
+
+    public void changeNames(MouseEvent mouseEvent) {
+        namesBox.setVisible(!randomNames.isSelected());
     }
 
     public void startGame(ActionEvent actionEvent) {
         int players= ( (Double) numOfPlayers.getValue() ).intValue();
         int cards= ( (Double) numOfCards.getValue() ).intValue();
-        boolean rainbow= (hasRainbows.getValue()==1.0);
-        boolean random= (randomOrder.getValue()==1.0);
+        boolean rainbow= hasRainbows.isSelected();
+        boolean random= randomOrder.isSelected();
 
         HanabiMain.setUpWindow.board = new Board(players, 40, 8, 8, cards, new Deck(true, rainbow, true),
                 random, Board.randomNames(players));
@@ -40,7 +81,5 @@ public class SetUpController {
         HanabiMain.setUpWindow.stage.close();
     }
 
-    public void advanced(MouseEvent mouseEvent) {
-        advancedSettings.setVisible(showAdvanced.isSelected());
-    }
+
 }
