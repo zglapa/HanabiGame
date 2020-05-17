42a3bc5c870887040e85d91dda6027d80eb1fb72
Roch Wojtowicz <rochw00@gmail.com>
2020-05-17 12:35:11 +0000

    New background

--- Hanabi/src/main/java/hanabi/Controller/MainMenuController.java
+++ Hanabi/src/main/java/hanabi/Controller/MainMenuController.java
@@ -1,13 +1,32 @@
 package hanabi.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
-public class MainMenuController {
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
     @FXML Button exitButton;
     @FXML Button playButton;
     @FXML Button settingsButton;
+    @FXML Rectangle background;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            background.setFill(new ImagePattern(new Image(getClass().getResource("/textures/image0.png").toURI().toString()),0,0,1,1,true));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        //image.setVisible(false);
+    }
 
     public void handleExitButton() {
         MainMenuWindow.action = 3;
